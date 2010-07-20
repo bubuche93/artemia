d58c9b98700caa7c37234ba0b65fd97b8e2bd3b4
angeluse <anterosxbox@gmail.com>
2010-07-20 17:24:44 +0200

    azefe

--- sites/all/themes/artemia/search-block-form.tpl.php
+++ sites/all/themes/artemia/search-block-form.tpl.php
@@ -1,9 +1,5 @@
 <?php
 
-
-  //    * Remove "Search this site: "  *    ////
-//  $search["search_theme_form"]= str_replace("Chercher dans ce site : ", "", $search["search_theme_form"]);
-
   print $search["search_theme_form"];
   print $search["submit"];
   print $search["hidden"];
