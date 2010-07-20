c6ac85772e4a5306826ab52795d0960ba732a0fb
angeluse <anterosxbox@gmail.com>
2010-07-20 16:51:01 +0200

    zef

--- sites/all/themes/artemia/search-block-form.tpl.php
+++ sites/all/themes/artemia/search-block-form.tpl.php
@@ -1,9 +1,9 @@
 <?php
   /* remove ' this site' - probably should try more surefire way using </label> */
-  $search["search_block_form"]= str_replace("Search this site:", "", $search["search_block_form"]);
+  $search["search_block_form"]= str_replace("Chercher dans ce site : ", "", $search["search_block_form"]);
 
   /* Replace button input type with image  */
-  $search["submit"]=str_replace('input type="submit"', 'input type="image" src="/search.gif" ', $search["submit"]);
+  $search["submit"]=str_replace('input type="submit"', 'input type="image" src="sites/all/themes/artemia/images/logo.png" ', $search["submit"]);
 
   print $search["search_block_form"];
   print $search["submit"];
