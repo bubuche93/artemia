8bd8de641b31052326be394b032c521ef9167152
angeluse <anterosxbox@gmail.com>
2010-07-20 17:15:00 +0200

    fezfe

--- sites/all/themes/artemia/block-search.tpl.php
+++ sites/all/themes/artemia/block-search.tpl.php
@@ -1,6 +1,4 @@
 <?php
 
-$search["search_theme_form"]=str_replace('value=""', 'value="Search..." onblur="setTimeout(\'closeResults()\',2000); if (this.value == \'\') {this.value = \'\';}"  onfocus="if (this.value == \'Search...\') {this.value = \'\';}" ', $search["search_theme_form"]);
 
-  
 ?>
