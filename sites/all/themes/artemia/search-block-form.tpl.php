96747547b4c55ec597fa2ecc4f21003b7c85d6d3
angeluse <anterosxbox@gmail.com>
2010-07-20 17:34:49 +0200

    yhy('

--- sites/all/themes/artemia/search-block-form.tpl.php
+++ sites/all/themes/artemia/search-block-form.tpl.php
@@ -1,8 +1,10 @@
 <?php
 
 
-  //    * Remove "Search this site: "  *    ////
-//  $search["search_theme_form"]= str_replace("Chercher dans ce site : ", "", $search["search_theme_form"]);
+
+ $search["search_block_form"]= str_replace("Chercher dans ce site : ", "", $search["search_block_form"]);
+ $search["search_block_form"]= str_replace('<div id="edit-search-block-form-1-wrapper" class="form-item">', "", $search["search_block_form"]);
+ $search["search_block_form"]= str_replace('</div>', "", $search["search_block_form"]);
 
   print $search["search_block_form"];
   print $search["submit"];
