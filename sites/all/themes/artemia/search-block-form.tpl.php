c94d67147b870c1723e02600c9da7cdbd194af0f
angeluse <anterosxbox@gmail.com>
2010-07-20 17:53:07 +0200

    enlever ce label ^^

--- sites/all/themes/artemia/search-block-form.tpl.php
+++ sites/all/themes/artemia/search-block-form.tpl.php
@@ -1,10 +1,11 @@
 <?php
 
 
+$search["search_block_form"]= str_replace("Search this site: ", "", $search["search_block_form"]);
 
- $search["search_block_form"]= str_replace('<label for="edit-search-block-form-1">Chercher dans ce site : </label>', "", $search["search_block_form"]);
- $search["search_block_form"]= str_replace('<div id="edit-search-block-form-1-wrapper" class="form-item">', "", $search["search_block_form"]);
- $search["search_block_form"]= str_replace('</div>', "", $search["search_block_form"]);
+$search["search_block_form"]= str_replace('<div id="edit-search-block-form-1-wrapper" class="form-item">', "", $search["search_block_form"]);
+
+$search["search_block_form"]= str_replace('</div>', "", $search["search_block_form"]);
 
   print $search["search_block_form"];
   print $search["submit"];
