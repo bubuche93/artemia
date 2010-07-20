e3fcb7ea997032a574b1392eb6da3b1369de08f5
angeluse <anterosxbox@gmail.com>
2010-07-20 18:33:03 +0200

    sdfe

--- sites/all/themes/artemia/search-block-form.tpl.php
+++ sites/all/themes/artemia/search-block-form.tpl.php
@@ -1,9 +1,6 @@
 <?php
 
 
-$search["search_block_form"]= str_replace('<label for="edit-search-block-form-1">', '<input type="hidden" name="', $search["search_block_form"]);
-$search["search_block_form"]= str_replace('</label>', '">', $search["search_block_form"]);
-
 $search["search_block_form"]= str_replace('<div id="edit-search-block-form-1-wrapper" class="form-item">', "", $search["search_block_form"]);
 
 $search["search_block_form"]= str_replace('</div>', "", $search["search_block_form"]);
