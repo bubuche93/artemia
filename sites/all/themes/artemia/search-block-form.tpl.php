42e944cf9a2a918607885cb2f9a8a00dbef1f9fc
angeluse <anterosxbox@gmail.com>
2010-07-20 18:25:42 +0200

    sezffz

--- sites/all/themes/artemia/search-block-form.tpl.php
+++ sites/all/themes/artemia/search-block-form.tpl.php
@@ -2,15 +2,15 @@
 
 
 
-  $prefix_string = 'Search this site';
+  $prefix_string = 'Chercher dans ce site';
   $transl_prefix_string = t($prefix_string). ':';
   $search['new_search_block_form'] = str_replace($transl_prefix_string, "", $search["search_block_form"]);
 
-$search["new2_search_block_form"]= str_replace('<div id="edit-search-block-form-1-wrapper" class="form-item">', "", $search["new_search_block_form"]);
+$search["new_search_block_form"]= str_replace('<div id="edit-search-block-form-1-wrapper" class="form-item">', "", $search["new_search_block_form"]);
 
-$search["new3_search_block_form"]= str_replace('</div>', "", $search["new2_search_block_form"]);
+$search["new_search_block_form"]= str_replace('</div>', "", $search["new_search_block_form"]);
 
-  print $search["new3_search_block_form"];
+  print $search["new_search_block_form"];
   print $search["submit"];
   print $search["hidden"];
 
