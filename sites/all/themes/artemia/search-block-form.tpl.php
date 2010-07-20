f87a2bf13f8f68c5c2918b1f04d0842e8e58bd32
angeluse <anterosxbox@gmail.com>
2010-07-20 18:03:07 +0200

    efzfez

--- sites/all/themes/artemia/search-block-form.tpl.php
+++ sites/all/themes/artemia/search-block-form.tpl.php
@@ -1,7 +1,6 @@
 <?php
 
 
-$search["search_block_form"]= str_replace('<label for="edit-search-block-form-1">Chercher dans ce site&nbsp;: </label>', "", $search["search_block_form"]);
 
 $search["search_block_form"]= str_replace('<label for="edit-search-block-form-1">Chercher dans ce site : </label>', "", $search["search_block_form"]);
 
