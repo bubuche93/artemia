c5ae433498237a6a6a00ec1abd550f5f5214fedd
angeluse <anterosxbox@gmail.com>
2010-07-20 17:46:20 +0200

    ffez

--- sites/all/themes/artemia/search-block-form.tpl.php
+++ sites/all/themes/artemia/search-block-form.tpl.php
@@ -2,9 +2,9 @@
 
 // Hook pour enlever le label précédant la recherche en Homepage et enlever le div qui englobe le champs de recherche
 
- $search["search_block_form"]= str_replace('<label for="edit-search-block-form-1">Chercher dans ce site : </label>', '', $search["search_block_form"]);
- $search["search_block_form"]= str_replace('<div id="edit-search-block-form-1-wrapper" class="form-item">', '', $search["search_block_form"]);
- $search["search_block_form"]= str_replace('</div>', '', $search["search_block_form"]);
+ $search["search_block_form"]= str_replace('<label for="edit-search-block-form-1">Chercher dans ce site : </label>', "", $search["search_block_form"]);
+ $search["search_block_form"]= str_replace('<div id="edit-search-block-form-1-wrapper" class="form-item">', "", $search["search_block_form"]);
+ $search["search_block_form"]= str_replace('</div>', "", $search["search_block_form"]);
 
   print $search["search_block_form"];
   print $search["submit"];
