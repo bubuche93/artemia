1ea65888545fed0bfd8951a9b36e5de7846fd9a0
angeluse <anterosxbox@gmail.com>
2010-07-20 16:49:09 +0200

    fez

--- sites/all/themes/artemia/search-block-form.tpl.php
+++ sites/all/themes/artemia/search-block-form.tpl.php
@@ -1,7 +1,13 @@
 <?php
+  /* remove ' this site' - probably should try more surefire way using </label> */
+  $search["search_block_form"]= str_replace("Search this site:", "", $search["search_block_form"]);
 
-  unset($variables['form']['search_theme_form']['#title']);
-  unset($variables['form']['search_theme_form']['#printed']);
-  
+  /* Replace button input type with image  */
+  $search["submit"]=str_replace('input type="submit"', 'input type="image" src="/search.gif" ', $search["submit"]);
+
+  print $search["search_block_form"];
+  print $search["submit"];
+  print $search["hidden"];
+ 
   
 ?>
