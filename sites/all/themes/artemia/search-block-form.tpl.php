0de44507c68d7a5a15a1e1afef50e56e7738efdb
angeluse <anterosxbox@gmail.com>
2010-07-20 17:49:36 +0200

    test enlever div

--- sites/all/themes/artemia/search-block-form.tpl.php
+++ sites/all/themes/artemia/search-block-form.tpl.php
@@ -1,13 +1,14 @@
 <?php
 
-// Hook pour enlever le label prŽcŽdant la recherche en Homepage et enlever le div qui englobe le champs de recherche
 
- $search["search_block_form"]= str_replace('<label for="edit-search-block-form-1">Chercher dans ce site : </label>', "", $search["search_block_form"]);
+
+ $search["search_block_form"]= str_replace('<label for="edit-search-block-form-1">Chercher dans ce site&nbsp;: </label>', "", $search["search_block_form"]);
  $search["search_block_form"]= str_replace('<div id="edit-search-block-form-1-wrapper" class="form-item">', "", $search["search_block_form"]);
  $search["search_block_form"]= str_replace('</div>', "", $search["search_block_form"]);
 
   print $search["search_block_form"];
   print $search["submit"];
   print $search["hidden"];
+
 
 ?>
