7f9ee2d5a9de84905e254a66a04f9e00e0198eb5
angeluse <anterosxbox@gmail.com>
2010-07-20 16:32:23 +0200

    dsds

--- sites/all/themes/artemia/search-block-form.tpl.php
+++ sites/all/themes/artemia/search-block-form.tpl.php
@@ -1,15 +1,10 @@
-<div id="search" class="container-inline">
   <?php
 
   /* remove ' this site' - probably should try more surefire way using </label> */
   $search["search_theme_form"]= str_replace(" this site", "", $search["search_theme_form"]);
-
-  /* Replace button input type with image  */
-  $search["submit"]=str_replace('input type="submit"', 'input type="image" src="yoursite-path-to-your-search.png" ', $search["submit"]);
 
   print $search["search_theme_form"];
   print $search["submit"];
   print $search["hidden"]; 
   
    ?>
-</div>
