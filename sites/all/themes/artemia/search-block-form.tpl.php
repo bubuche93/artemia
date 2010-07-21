2d478ab02f35757e1f10faaf371d5f3e7683bbbe
angeluse <anterosxbox@gmail.com>
2010-07-21 13:59:57 +0200

    efz

--- sites/all/themes/artemia/search-block-form.tpl.php
+++ sites/all/themes/artemia/search-block-form.tpl.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+* Override of the search block form
+*
+*/
+
 
 $search["search_block_form"]= str_replace('<label for="edit-search-block-form-1">', '<input type="hidden" name="', $search["search_block_form"]);
 $search["search_block_form"]= str_replace('</label>', '">', $search["search_block_form"]);
@@ -7,6 +12,8 @@ $search["search_block_form"]= str_replace('</label>', '">', $search["search_bloc
 $search["search_block_form"]= str_replace('<div id="edit-search-block-form-1-wrapper" class="form-item">', "", $search["search_block_form"]);
 
 $search["search_block_form"]= str_replace('</div>', "", $search["search_block_form"]);
+
+$search["search_block_form"]= str_replace('id="edit-search-block-form-1"', 'id="edit-search-block-form-1" class="inputtextsearch"', $search["search_block_form"]);
 
   print $search["search_block_form"];
   print $search["submit"];
